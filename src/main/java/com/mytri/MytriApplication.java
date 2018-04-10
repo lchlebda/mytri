3ce0ee5912a7f18db18c45c49b98a5767843d7ff
Lukasz Chlebda <lchlebda@gmail.com>
2018-04-11 00:37:19 +0200

    create project

---
+++ src/main/java/com/mytri/MytriApplication.java
@@ -0,0 +1,12 @@
+package com.mytri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MytriApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MytriApplication.class, args);
+    }
+}
